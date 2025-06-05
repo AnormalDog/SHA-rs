311cc59b059972c23ebe8d58608a38f0eb2a2fb8
AnormalDog <150300262+AnormalDog@users.noreply.github.com>
2025-06-05 14:52:55 +0100

    fixed format with cargo fmt

--- src/basic_functions.rs
+++ src/basic_functions.rs
@@ -1,36 +1,36 @@
 // size (in bits) of a u64
-const U64SIZE : usize = size_of::<u64>() * 8;
+const U64SIZE: usize = size_of::<u64>() * 8;
 
 // size (in bits) of a u32
-const U32SIZE : usize = size_of::<u32>() * 8;
+const U32SIZE: usize = size_of::<u32>() * 8;
 
 // The right shift. As a function for easier syntax
 
 pub trait BasicFunctions {
-  fn right_shift(&self, n : usize) -> Self;
-  fn right_rotation(&self, n : usize) -> Self;
+    fn right_shift(&self, n: usize) -> Self;
+    fn right_rotation(&self, n: usize) -> Self;
 }
 
 impl BasicFunctions for u64 {
-  fn right_rotation(&self, n : usize) -> Self {
-    assert!(n < U64SIZE);
-    (self >> n) | (self << (U64SIZE - n))
-  }
+    fn right_rotation(&self, n: usize) -> Self {
+        assert!(n < U64SIZE);
+        (self >> n) | (self << (U64SIZE - n))
+    }
 
-  fn right_shift(&self, n : usize) -> Self {
-    assert!(n < U64SIZE);
-    self >> n
-  }  
+    fn right_shift(&self, n: usize) -> Self {
+        assert!(n < U64SIZE);
+        self >> n
+    }
 }
 
 impl BasicFunctions for u32 {
-  fn right_rotation(&self, n : usize) -> Self {
-    assert!(n < U32SIZE);
-    (self >> n) | (self << (U32SIZE - n))
-  }
+    fn right_rotation(&self, n: usize) -> Self {
+        assert!(n < U32SIZE);
+        (self >> n) | (self << (U32SIZE - n))
+    }
 
-  fn right_shift(&self, n : usize) -> Self {
-    assert!(n < U32SIZE);
-    self >> n
-  }  
+    fn right_shift(&self, n: usize) -> Self {
+        assert!(n < U32SIZE);
+        self >> n
+    }
 }
